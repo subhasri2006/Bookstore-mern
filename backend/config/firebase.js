78b003733f89edfb34b5043de44072842c99f44c
Subha Sri <subhasri4844@gmail.com>
2026-07-14 00:05:42 +0530

    Update backend dependencies

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin");
-
+console.log("Firebase key exists:", !!process.env.FIREBASE_PRIVATE_KEY);
 const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
