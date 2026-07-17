f781c16f1a56d31e95e947b76293b501edf906ef
Subha Sri <subhasri4844@gmail.com>
2026-07-17 14:03:44 +0530

    Update frontend API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-render-backend-url.onrender.com"
+  baseURL: "https://bookstore-backend-7brx.onrender.com"
 });
 
 // attach token automatically
