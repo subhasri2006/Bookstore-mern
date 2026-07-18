f732965351f504f8861cd7603c0c54bb13a1295b
Subha Sri <subhasri4844@gmail.com>
2026-07-18 20:21:44 +0530

    Fix API base URL for backend routes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://bookstore-backend-7brx.onrender.com"
+  baseURL: "https://bookstore-backend-7brx.onrender.com/api",
 });
 
 // attach token automatically
@@ -16,3 +16,21 @@ API.interceptors.request.use((req) => {
 });
 
 export default API;
+/*import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://bookstore-backend-7brx.onrender.com"
+});
+
+// attach token automatically
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
+});
+
+export default API;*/
